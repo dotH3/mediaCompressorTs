35c07eae6ae793db36690004a28c3e58fe19db9a
h3 <joaquinbastias321@gmail.com>
2023-02-24 13:56:24 -0300

    update

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 import fs from 'fs'
 import sharp from 'sharp'
 
-const buffer = fs.readFileSync('./src/input/img.jpeg')
 
 const edit = async()=>{
-  const img = await sharp(buffer).rotate(90).toBuffer();
+  const buffer = fs.readFileSync('./src/input/img.jpeg')
+  console.log((Buffer.byteLength(buffer) / (1024 * 1024)).toFixed(2)+' MB');
+  
+  const img = await sharp(buffer).resize({width:800,height:600,fit:'contain'}).toBuffer();
+  console.log((Buffer.byteLength(img) / (1024 * 1024)).toFixed(2)+' MB');
 
   await fs.writeFile('./src/output/img.jpg',img,(err)=>{
     if(err)return console.log(err)
-    console.log('!!!');
   })
 
-  return img
+  return console.log('Done!');
+  
 }
 
 edit();
